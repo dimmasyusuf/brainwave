303e66fcc4e160f3dfe5f612650d0736c5b0b584
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-04-02 02:23:35 +0700

    feat: create collaboration ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
     </>
   );
