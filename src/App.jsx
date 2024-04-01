e3b88b2551031bacb96c5b1ba9bbb25153b82442
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-04-02 02:12:22 +0700

    feat: create hero ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Header from './components/Header';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
         <Header />
+        <Hero />
       </div>
     </>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import ButtonSvg from '../assets/svg/ButtonSvg';
+import ButtonSvg from "../assets/svg/ButtonSvg";
 
 export default function Button({
   className,
@@ -9,25 +9,19 @@ export default function Button({
   white,
 }) {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
-    px || 'px-7'
-  } ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`;
-  const spanClasses = 'relative z-10';
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button
-      className={classes}
-      onClick={onClick}
-    >
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
   const renderLink = () => (
-    <a
-      href={href}
-      className={classes}
-    >
+    <a href={href} className={classes}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
