1739d1a59195479abc4d445cee6580cc7bf95585
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-04-02 02:26:03 +0700

    feat: create services ui component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
     </>
   );
